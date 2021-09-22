03ee95cbc37af0471560d7ed3729498e30ba9609
Ian Mancini <ianmethyst@gmail.com>
2021-09-22 00:59:44 -0300

    Add shortcut for org-agenda and ESC mapping for nvim terminals

--- filesets/base/nvim/.config/nvim/lua/plugins/nvim-toggleterm.lua
+++ filesets/base/nvim/.config/nvim/lua/plugins/nvim-toggleterm.lua
@@ -1,6 +1,6 @@
 require("toggleterm").setup{
     open_mapping = [[<a-`>]],
-    hide_numbers = true, 
+    hide_numbers = true,
     shade_terminals = true,
     shading_factor = 1,
     start_in_insert = true,
@@ -11,3 +11,9 @@ require("toggleterm").setup{
         border = 'single',
     }
 }
+
+function _G.set_terminal_keymaps()
+    utils.map('t', '<esc>', [[<C-\><C-n>]])
+end
+
+vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
